bdf7543c07d22c41019154f56b9fb2ca22106644
kirigaikabuto <tleugazy98@gmail.com>
2024-03-02 19:39:35 +0500

    sdsdd

--- projects/project1/cmd/main.go
+++ projects/project1/cmd/main.go
@@ -7,15 +7,24 @@ import (
 	"github.com/kirigaikabuto/hero_preparation/projects/project1/pkg/handler"
 	"github.com/kirigaikabuto/hero_preparation/projects/project1/pkg/repository"
 	"github.com/kirigaikabuto/hero_preparation/projects/project1/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error install configuration file %s", err.Error())
+	}
 	repo := repository.NewRepository()
 	services := service.NewService(repo)
 	handlers := handler.NewHandler(services)
 	srv := new(project1.Server)
-
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error running of server %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
---
+++ projects/project2/go.mod
@@ -0,0 +1,3 @@
+module github.com/kirigaikabuto/hero_preparation/projects/project2
+
+go 1.22.0
