2a61d1b6720fa4caddb7900ccf4436cc79aa4bd1
kirigaikabuto <tleugazy98@gmail.com>
2024-03-03 23:26:00 +0500

    sdsdsdsd

--- projects/project1/cmd/main.go
+++ projects/project1/cmd/main.go
@@ -2,24 +2,50 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/kirigaikabuto/hero_preparation/projects/project1"
 	"github.com/kirigaikabuto/hero_preparation/projects/project1/pkg/handler"
 	"github.com/kirigaikabuto/hero_preparation/projects/project1/pkg/repository"
 	"github.com/kirigaikabuto/hero_preparation/projects/project1/pkg/service"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 )
 
 func main() {
 	if err := initConfig(); err != nil {
 		log.Fatalf("error install configuration file %s", err.Error())
+		return
 	}
-	repo := repository.NewRepository()
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error during reading from .env file %s", err.Error())
+		return
+	}
+	db, err := repository.NewPostgres(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("db_password"),
+		DBName:   viper.GetString("db.db_name"),
+		SSLMode:  viper.GetString("db.ssl_mode"),
+	})
+	if err != nil {
+		log.Fatalf("error during connection to postgres %s", err.Error())
+		return
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("error during ping of postgres %s", err.Error())
+		return
+	}
+	repo := repository.NewRepository(db)
 	services := service.NewService(repo)
 	handlers := handler.NewHandler(services)
 	srv := new(project1.Server)
 	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error running of server %s", err.Error())
+		return
 	}
 }
 
---
+++ projects/project1/pkg/repository/postgres.go
@@ -0,0 +1,26 @@
+package repository
+
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+func NewPostgres(cfg Config) (*sqlx.DB, error) {
+	db, err := sqlx.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.Username, cfg.Password, cfg.DBName, cfg.SSLMode,
+	))
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
